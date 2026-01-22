ed2700d5269ae921aed76d2150c514874b6ce532
SheillaO <sheilla.alusa@strathmore.edu>
2026-01-22 15:02:03 +0300

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,19 +8,8 @@ export default function Dictionary() {
   let [results, setresults] = useState(null);
 
   function handleresponse(response) {
-    console.log(response.data); 
+    console.log(response.data);
     setresults(response.data);
-
-    
-
-    setWordData({
-      word: data.word,
-      phonetic: data.phonetic,
-      partOfSpeech: firstMeaning.partOfSpeech,
-      definition: firstMeaning.definition, 
-      example: firstMeaning.example || null, 
-      synonyms: firstMeaning.synonyms || [], 
-    });
   }
 
   function search(event) {
@@ -47,30 +36,6 @@ export default function Dictionary() {
         />
       </form>
       <Results results={results} />
-      {wordData && (
-        <div className="results">
-          <h2>{wordData.word}</h2>
-          <p>
-            <strong>Phonetic:</strong> {wordData.phonetic}
-          </p>
-          <p>
-            <strong>Part of speech:</strong> {wordData.partOfSpeech}
-          </p>
-          <p>
-            <strong>Definition:</strong> {wordData.definition}
-          </p>
-          {wordData.example && (
-            <p>
-              <em>Example: {wordData.example}</em>
-            </p>
-          )}
-          {wordData.synonyms.length > 0 && (
-            <p>
-              <strong>Synonyms:</strong> {wordData.synonyms.join(", ")}
-            </p>
-          )}
-        </div>
-      )}
     </div>
   );
 }
