ae2151aec52534c3bbddfc809647e66371c30ab9
SheillaO <sheilla.alusa@strathmore.edu>
2026-01-22 11:49:25 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./Catalyst_Marketing_Lab_Logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <header className="py-3 border-bottom">
+      <div className="container">
+        <div className="d-flex justify-content-between align-items-center mt-3 ps-2">
+          <img src={logo} className="brand-logo" alt="Catalyst Marketing Lab" />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default App;
