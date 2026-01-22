2e31082dc3305f26fde1f1b212c5d9a63d837e28
SheillaO <sheilla.alusa@strathmore.edu>
2026-01-22 12:04:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   return (
-    <header className="py-3 border-bottom">
+    <header className="py-3">
       <div className="container">
         <div className="d-flex justify-content-between align-items-center mt-3 ps-2">
           <img src={logo} className="brand-logo" alt="Catalyst Marketing Lab" />
