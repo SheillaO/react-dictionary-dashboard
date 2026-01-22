e0cd0e770bfed9cf5f390ded4d760c43e1bac052
SheillaO <sheilla.alusa@strathmore.edu>
2026-01-22 13:58:52 +0300

    Console logged the api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,21 +4,29 @@ import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  const [wordData, setWordData] = useState(null);
 
-
-function handleresponse(response){
+  function handleresponse(response) {
     console.log(response.data[0]);
-    console.log(response.data[0].meanings[0].definitions[0]);
+    const firstMeaning = data.meanings[0];
+    const firstDefinition = firstMeaning.definitions[0];
 
-}
+    setWordData({
+      word: data.word,
+      phonetic: data.phonetic,
+      partOfSpeech: firstMeaning.partOfSpeech,
+      definition: firstDefinition.definition,
+      example: firstDefinition.example || null,
+      synonyms: firstDefinition.synonyms || [],
+    });
+  }
 
   function search(event) {
     event.preventDefault();
 
-let apikey = "7601b0fff0179o9d5059a8db34ctbc66";
-let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apikey}`;
-axios.get(apiUrl).then(handleresponse);
-
+    let apikey = "7601b0fff0179o9d5059a8db34ctbc66";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apikey}`;
+    axios.get(apiUrl).then(handleresponse);
   }
 
   function handleKeywordChange(event) {
@@ -35,6 +43,30 @@ axios.get(apiUrl).then(handleresponse);
           autoFocus={true}
         />
       </form>
+      {wordData && (
+        <div className="results">
+          <h2>{wordData.word}</h2>
+          <p>
+            <strong>Phonetic:</strong> {wordData.phonetic}
+          </p>
+          <p>
+            <strong>Part of speech:</strong> {wordData.partOfSpeech}
+          </p>
+          <p>
+            <strong>Definition:</strong> {wordData.definition}
+          </p>
+          {wordData.example && (
+            <p>
+              <em>Example: {wordData.example}</em>
+            </p>
+          )}
+          {wordData.synonyms.length > 0 && (
+            <p>
+              <strong>Synonyms:</strong> {wordData.synonyms.join(", ")}
+            </p>
+          )}
+        </div>
+      )}
     </div>
   );
 }
