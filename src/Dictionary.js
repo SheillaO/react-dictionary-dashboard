36c832003ba733cfe9f90102809e80ab843bec3e
SheillaO <sheilla.alusa@strathmore.edu>
2026-01-23 16:30:18 +0300

    Mades tweaks on the CSS and added the Pexels photos api
    Pexel api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setresults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleresponse(response) {
+  function handleDictionaryresponse(response) {
     console.log(response.data);
     setresults(response.data);
   }
@@ -17,8 +19,19 @@ export default function Dictionary(props) {
     //shecodes dictionary api
     let apikey = "7601b0fff0179o9d5059a8db34ctbc66";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apikey}`;
-    axios.get(apiUrl).then(handleresponse);
+    axios.get(apiUrl).then(handleDictionaryresponse);
   }
+
+  let pexelsApiKey = "cNmNSmAGYVSHrL3gDxKOHw5d7KVfoMLn2dPuDqL66nbqbwW2ftj5oTxH";
+  let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+  let headers = {Authorization: `Bearer ${pexelsApiKey}`};
+
+  axios.get(pexelsApiUrl, {headers: headers}).then(handlePexelsresponse);
+
+  function handlePexelsresponse(response) {
+    setPhotos(response.data.photos)
+  }
+
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -29,7 +42,7 @@ export default function Dictionary(props) {
     setKeyword(event.target.value);
   }
 
-  function load(){
+  function load() {
     setLoaded(true);
     search();
   }
@@ -53,6 +66,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
